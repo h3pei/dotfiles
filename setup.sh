163218b6177dfa5e98367e21ec54423539ecca2e
mogulla3 <hs.iarn3@gmail.com>
2014-05-02 16:18:32 +0000

    Update setup.sh
    - submoduleの初期化処理の追加
    - シンボリックリンクを貼る際にforループを使うように変更

--- setup.sh
+++ setup.sh
@@ -1,26 +1,17 @@
 #!/bin/sh
 
+conf_files=(".vimrc" ".vim" ".zshrc" ".tmux.conf" ".gitconfig")
+
+git submodule init
+git submodule update
+
 cd
 
-if [ ! -e ~/.vimrc ]; then
-  ln -s ~/dotfiles/.vimrc
-fi
-
-if [ ! -e ~/.vim ]; then
-  ln -s ~/dotfiles/.vim
-fi
-
-if [ ! -e ~/.zshrc ]; then
-  ln -s ~/dotfiles/.zshrc
-fi
-
-if [ ! -e ~/.tmux.conf ]; then
-  ln -s ~/dotfiles/.tmux.conf
-fi
-
-if [ ! -e ~/.gitconfig ]; then
-  ln -s ~/dotfiles/.gitconfig
-fi
+for conf_file in ${conf_files[@]}; do
+  if [ ! -e $conf_file ]; then
+    ln -s ~/dotfiles/$conf_file
+  fi
+done
 
 if [ ! -e ~/.vim/dotfiles/.vim/bundle/neobundle.vim ]; then
   git clone https://github.com/Shougo/neobundle.vim ~/dotfiles/.vim/bundle/neobundle.vim
