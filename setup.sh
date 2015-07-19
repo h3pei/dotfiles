f19106b0c9b98d60c2260930df72497b070e5d85
mogulla3 <hs.iarn3@gmail.com>
2015-07-19 11:21:51 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
+set -ex
 
-conf_files=(".vimrc" ".vim" ".zshrc" ".tmux.conf" ".gitconfig")
-
+# oh-my-zsh
 git submodule init
 git submodule update
 
 cd
 
+conf_files=(".vimrc" ".vim" ".zshrc" ".tmux.conf" ".gitconfig")
 for conf_file in ${conf_files[@]}; do
   if [ ! -e $conf_file ]; then
     ln -s ~/dotfiles/$conf_file
   fi
 done
 
+# vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
