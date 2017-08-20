c8a47e6956621739985f98810257594111ebef69
mogulla3 <hs.iarn3@gmail.com>
2017-08-20 10:31:36 +0900

    Make setup.sh more flexible

--- setup.sh
+++ setup.sh
@@ -1,21 +1,41 @@
 #!/bin/sh
 set -ex
 
+mkdir -p $HOME/bin
+mkdir -p $HOME/trash
+mkdir -p $HOME/.ssh
+
 # prezto
-git clone --recursive https://github.com/mogulla3/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+if [ ! -e "${ZDOTDIR:-$HOME}/.zprezto" ]; then
+  git clone --recursive https://github.com/mogulla3/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
 
 # ruby dict
-git clone https://github.com/pocke/dicts.git ~/dotfiles/.vim/dicts/dicts
+if [ ! -e "$HOME/dotfiles/dicts/dicts" ]; then
+  git clone https://github.com/pocke/dicts.git ~/dotfiles/dicts/dicts
+fi
 
 cd
 
-conf_files=(".vimrc" ".vim" ".tmux.conf" ".gitconfig" ".zshrc" ".zpreztorc" ".zlogin" ".zshenv" ".zprofile")
-for conf_file in ${conf_files[@]}; do
-  if [ ! -e $conf_file ]; then
-    ln -s ~/dotfiles/$conf_file
+dotfiles=(".vimrc" ".vim" ".tmux.conf" ".gitconfig" ".zshrc" ".zpreztorc" ".zlogin" ".zshenv" ".zprofile")
+for dotfile in ${dotfiles[@]}; do
+  # Remove dotfile if exists
+  if [ -e "${HOME}/${dotfile}" ]; then
+    if [ -L "${HOME}/${dotfile}" ]; then
+      unlink $HOME/$dotfile
+    fi
+
+    if [ -f "${HOME}/${dotfile}" ]; then
+      rm -r $HOME/$dotfile
+    fi
+
+    if [ -d "${HOME}/${dotfile}" ]; then
+      rm -r $HOME/$dotfile
+    fi
   fi
+
+  ln -s $HOME/dotfiles/$dotfile $HOME/$dotfile
 done
 
 # vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
