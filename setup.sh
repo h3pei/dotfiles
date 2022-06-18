e743881a6d3c0fccfbfa3f77f806c8398e8c8507
mogulla3 <hs.iarn3@gmail.com>
2022-06-18 23:50:30 +0900

    Rewrite setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 set -ex
 
 mkdir -p $HOME/work
@@ -7,50 +8,24 @@ mkdir -p $HOME/trash
 mkdir -p $HOME/memo
 mkdir -p $HOME/screenshots
 mkdir -p $HOME/.ssh
-mkdir -p $HOME/.config/nvim/ftplugin
+mkdir -p $HOME/.config/nvim
 
-cd
-
-dotfiles=(
-  ".tmux.conf"
-  ".tigrc"
-  ".gitconfig"
-  ".zshrc"
-  ".zlogin"
-  ".zshenv"
-  ".zprofile"
-  ".pryrc"
-  ".gemrc"
-  ".solargraph.yml"
-)
-
-for dotfile in ${dotfiles[@]}; do
-  # Remove dotfile if exists
-  if [ -e "${HOME}/${dotfile}" ]; then
-    if [ -L "${HOME}/${dotfile}" ]; then
-      unlink $HOME/$dotfile
-    fi
-
-    if [ -f "${HOME}/${dotfile}" ]; then
-      rm -r $HOME/$dotfile
-    fi
-
-    if [ -d "${HOME}/${dotfile}" ]; then
-      rm -r $HOME/$dotfile
-    fi
+# dotfiles 直下のドットファイル
+for dotfile in `ls -1a . | grep "^\." | grep -v '.gitignore'`; do
+  if [ -f $dotfile ]; then
+    ln -s -f `realpath $dotfile` $HOME/$dotfile
   fi
-
-  ln -s $HOME/dotfiles/$dotfile $HOME/$dotfile
 done
 
 # Neovim
-nvim_files=("init.vim" "coc-settings.json")
-for nvim_file in ${nvim_files[@]}; do
-  ln -s -f $HOME/dotfiles/.config/nvim/$nvim_file $HOME/.config/nvim/$nvim_file
-done
-for file in $HOME/dotfiles/.config/nvim/ftplugin/*.vim ; do
-  ln -s -f $file $HOME/.config/nvim/ftplugin/`basename $file`
-done
+ln -s -f -n `realpath .config/nvim` $HOME/.config/
+
+# rbenv
+if [ -d $HOME/.rbenv ]; then
+  for file in `ls -1 .rbenv/*`; do
+    ln -s -f `realpath $file` $HOME/$file
+  done
+fi
 
 # vim-plug
 if [ ! -e "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ]; then
@@ -58,6 +33,6 @@ if [ ! -e "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ]; 
 fi
 
 # zplug
-if [ ! -e "${HOME}/.zplug" ]; then
+if [ ! -d "${HOME}/.zplug" ]; then
   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 fi
