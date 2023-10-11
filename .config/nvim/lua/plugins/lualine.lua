71f2f5f37e5a5d8eb03a83ada0278472bc1b223d
mogulla3 <hs.iarn3@gmail.com>
2023-10-12 08:11:25 +0900

    [vim] Change lualine config
    display absolute path of current file.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,8 +7,9 @@ require("lualine").setup({
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { "filename" },
-    lualine_c = { "branch", "diff", "diagnostics" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    -- path = 3: Absolute path, with tilde as the home directory
+    lualine_c = { { "filename", path = 3 } },
     lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = { "progress" },
     lualine_z = { "location" },
