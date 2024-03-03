6053b8e3dd57816c36d0f63204a89f45a2066b6c
mogulla3 <hs.iarn3@gmail.com>
2024-03-03 18:24:40 +0900

    [vim] Add FJ command

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -29,3 +29,5 @@ vim.api.nvim_create_user_command("Profile", function()
   vim.cmd("profile func *")
   vim.cmd("profile file *")
 end, {})
+
+vim.api.nvim_create_user_command("FJ", "FactoryBotJump", {})
