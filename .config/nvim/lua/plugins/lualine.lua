6988ef21ae9ed97f043b0b92b564bb084c0d0b7b
mogulla3 <hs.iarn3@gmail.com>
2024-03-09 12:42:50 +0900

    [vim] Fix lualine sections

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -11,7 +11,7 @@ require("lualine").setup({
     -- path = 3: Absolute path, with tilde as the home directory
     lualine_c = { { "filename", path = 3 } },
     lualine_x = { "encoding", "fileformat", "filetype" },
-    lualine_y = { "progress" },
-    lualine_z = { "location" },
+    lualine_y = { "location" },
+    lualine_z = {},
   },
 })
