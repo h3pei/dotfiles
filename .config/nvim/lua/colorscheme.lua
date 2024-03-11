57bb3be7e6f6cffefe243b382170471b8f2ef4f5
mogulla3 <hs.iarn3@gmail.com>
2024-03-11 23:01:07 +0900

    [vim] Adjust colorscheme

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,6 +1,6 @@
 require("kanagawa").setup({
   transparent = true,
-  theme = "wave",
+  theme = "dragon",
   background = {
     dark = "dragon",
   },
@@ -15,3 +15,11 @@ vim.cmd("highlight BufferCurrent guibg=NONE")
 vim.cmd("highlight BufferCurrentSign guifg=#00dc82 guibg=NONE")
 -- current buffer が編集状態になるときの背景色とウィンドウの背景色を同化
 vim.cmd("highlight BufferCurrentMod guibg=NONE")
+
+-- NOTE: 以下は kanagawa.nvim 固有かもしれない
+-- SignColumn, LineNr, GitSigns の背景色とウィンドウの背景色を同化
+vim.cmd("highlight SignColumn guibg=NONE")
+vim.cmd("highlight LineNr guibg=NONE")
+vim.cmd("highlight GitSignsAdd guibg=NONE")
+vim.cmd("highlight GitSignsChange guibg=NONE")
+vim.cmd("highlight GitSignsDelete guibg=NONE")
