75413e091be34e8a2dd716df182c1f8137306e54
mogulla3 <hs.iarn3@gmail.com>
2024-03-30 22:54:39 +0900

    [vim] Fix kanagawa.nvim color settings

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -5,8 +5,18 @@ require("kanagawa").setup({
     dark = "wave",
   },
   colors = {
-    palette = {
-      fujiWhite = "#ffffff"
+    -- see:
+    -- - https://github.com/rebelot/kanagawa.nvim?tab=readme-ov-file#customization
+    -- - https://github.com/rebelot/kanagawa.nvim/blob/master/lua/kanagawa/themes.lua
+    -- - https://github.com/rebelot/kanagawa.nvim/blob/master/lua/kanagawa/colors.lua
+    theme = {
+      wave = {
+        ui = {
+          fg = "#ffffff", -- 文字色の変更
+          bg_visual = "#0d408c", -- 範囲選択時の背景色の変更
+          bg_gutter = "none", -- signcolumn の背景色の変更
+        }
+      }
     }
   }
 })
@@ -20,11 +30,3 @@ vim.cmd("highlight BufferCurrent guibg=NONE")
 vim.cmd("highlight BufferCurrentSign guifg=#00dc82 guibg=NONE")
 -- current buffer が編集状態になるときの背景色とウィンドウの背景色を同化
 vim.cmd("highlight BufferCurrentMod guibg=NONE")
-
--- NOTE: 以下は kanagawa.nvim 固有かもしれない
--- SignColumn, LineNr, GitSigns の背景色とウィンドウの背景色を同化
-vim.cmd("highlight SignColumn guibg=NONE")
-vim.cmd("highlight LineNr guibg=NONE")
-vim.cmd("highlight GitSignsAdd guibg=NONE")
-vim.cmd("highlight GitSignsChange guibg=NONE")
-vim.cmd("highlight GitSignsDelete guibg=NONE")
