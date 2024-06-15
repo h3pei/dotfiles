c3dc2bddd6dae25c4ba5410f0847da4b53fad3f6
mogulla3 <hs.iarn3@gmail.com>
2024-06-15 21:27:20 +0900

    [vim] Add ToggleDefaultTerminal

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -10,9 +10,21 @@ local tigTerminal = Terminal:new({
     vim.api.nvim_buf_set_keymap(term.bufnr, "t", ";t", "<CMD>close<CR>", { noremap = true, silent = true })
   end,
 })
+local defaultTerminal = Terminal:new({
+  dir = "git_dir",
+  direction = "float",
+  on_open = function(term)
+    vim.api.nvim_buf_set_keymap(term.bufnr, "t", ";T", "<CMD>close<CR>", { noremap = true, silent = true })
+  end,
+})
 
 function ToggleTigTerminal()
   tigTerminal:toggle()
 end
 
+function ToggleDefaultTerminal()
+  defaultTerminal:toggle()
+end
+
 vim.api.nvim_set_keymap("n", ";t", "<cmd>lua ToggleTigTerminal()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", ";T", "<cmd>lua ToggleDefaultTerminal()<CR>", { noremap = true, silent = true })
