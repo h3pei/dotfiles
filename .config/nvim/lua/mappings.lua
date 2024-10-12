954d52bcf2d9759ad795d917f8d9ee161213aef7
mogulla3 <hs.iarn3@gmail.com>
2024-10-12 20:36:23 +0900

    [vim] Add folding keymaps

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -28,11 +28,16 @@ vim.keymap.set("i", "<C-l>", function()
   return "<C-w>" .. result:upper()
 end, { expr = true })
 
--- Toggle folding
+-- folding
+-- * zo: カーソル位置の折り畳みを1段開く
+-- * zc: カーソル位置の折り畳みを1段閉じる
+-- * zR: 全ての折り畳みを開く
+-- * zM: 全ての折り畳みを閉じる
+-- https://vim-jp.org/vimdoc-ja/fold.html#fold-behavior
 vim.keymap.set("n", "<Tab>", "zo", noremap_and_silent)
 vim.keymap.set("n", "<S-Tab>", "zc", noremap_and_silent)
-vim.keymap.set("n", "<Leader>ff", "za", noremap_and_silent)
-vim.keymap.set("n", "<Leader>F", "<Cmd>%foldopen!<CR>", noremap_and_silent)
+vim.keymap.set("n", "<Leader><Tab>", "zR", noremap_and_silent)
+vim.keymap.set("n", "<Leader><S-Tab>", "zM", noremap_and_silent)
 
 -- Clear search highlighting
 vim.keymap.set("n", "<Leader>h", ":noh<CR>")
