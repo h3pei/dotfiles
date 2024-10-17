f102b38234f705c20dfaadf7d954bcb076d1fc75
mogulla3 <hs.iarn3@gmail.com>
2024-10-18 08:50:48 +0900

    [vim] Fix none-ls warning

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,8 +1,8 @@
 local null_ls = require("null-ls")
+
 null_ls.setup({
   log_level = "info",
   sources = {
-    null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.sql_formatter,
