3bcb5a2776b1be1d05b37e0d94fda5f6d063cf57
mogulla3 <hs.iarn3@gmail.com>
2025-02-22 13:09:48 +0900

    [vim] prettier -> biome

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -4,7 +4,7 @@ null_ls.setup({
   log_level = "info",
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.biome,
     null_ls.builtins.formatting.sql_formatter,
   },
 })
