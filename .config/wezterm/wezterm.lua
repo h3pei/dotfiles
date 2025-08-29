18d85ab109479c2e786f596dbf6ffa076076aa0b
h3pei <hs.iarn3@gmail.com>
2025-08-29 10:56:06 +0900

    [wezterm] Support docker-desktop custom URL scheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,27 +12,13 @@ wezterm.on("toggle-opacity", function(window, _)
   window:set_config_overrides(overrides)
 end)
 
-return {
+local config = {
   adjust_window_size_when_changing_font_size = false,
   color_scheme = "Hybrid (terminal.sexy)",
   font = wezterm.font("Cica"),
   font_size = 18.0,
   hide_tab_bar_if_only_one_tab = true,
-  hyperlink_rules = {
-    -- Linkify things that look like URL.
-    -- The wezterm default config doesn't support `localhost`, so I customized it.
-    -- see: https://wezfurlong.org/wezterm/hyperlinks.html#implicit-hyperlinks
-    {
-      regex = [[\bhttps?://[\w.-]+\S*\b]],
-      format = '$0',
-    },
-    -- Linkify things that look like URLs with numeric addresses as hosts.
-    -- Based on examples in the wezterm doc.
-    {
-      regex = [[\bhttps?://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-      format = '$0',
-    },
-  },
+  hyperlink_rules = wezterm.default_hyperlink_rules(),
   keys = {
     { key = "u", mods = "CMD", action = wezterm.action.EmitEvent("toggle-opacity") },
     { key = ",", mods = "CMD", action = wezterm.action.SpawnCommandInNewTab({ args = { "/opt/homebrew/bin/nvim", os.getenv("WEZTERM_CONFIG_FILE") } }) },
@@ -56,3 +42,12 @@ return {
     bottom = '0.3cell',
   }
 }
+
+-- docker-desktop のカスタムURLスキーマに対応
+-- 例: docker-desktop://dashboard/build/desktop-linux/desktop-linux/xxx
+table.insert(config.hyperlink_rules, {
+  regex = 'docker-desktop://[\\w\\-\\._~:/?#\\[\\]@!$&\'()*+,;=]+',
+  format = '$0',
+})
+
+return config
