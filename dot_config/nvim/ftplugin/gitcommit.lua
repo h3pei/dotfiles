d19177a16ee1a14c1f4bbc48e4d08b9e679d76d2
h3pei <hs.iarn3@gmail.com>
2026-02-28 13:07:11 +0900

    Run stylua

--- dot_config/nvim/ftplugin/gitcommit.lua
+++ dot_config/nvim/ftplugin/gitcommit.lua
@@ -5,11 +5,13 @@ vim.keymap.set("n", "C", function()
     return
   end
 
-  vim.notify('Generating commit message by claude...', vim.log.levels.INFO)
+  vim.notify("Generating commit message by claude...", vim.log.levels.INFO)
 
   local prompt =
     [[Generate ONLY a one-line Git commit message in Japanese, summarizing what was changed and why, based strictly on the contents of `git diff --cached`. Do not add explanation or a body. Output only the commit summary line.']]
-  local result = vim.system({ "claude", "--model", "haiku", "-p", prompt }, { text = true, env = { DISABLE_CLAUDE_NOTIFICATION = 1 } }):wait()
+  local commands = { "claude", "--model", "haiku", "-p", prompt }
+
+  local result = vim.system(commands, { text = true, env = { DISABLE_CLAUDE_NOTIFICATION = 1 } }):wait()
   local commit_message = result.stdout:gsub("\n", "")
   vim.api.nvim_buf_set_lines(0, 0, 0, false, { commit_message })
 end, { buffer = true, desc = "Generate commit message with Claude" })
